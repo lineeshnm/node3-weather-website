5901e7b4d45f0264dee7eba7786b48f7951c04ec
Lineesh NM <lineesh2009@gmail.com>
2020-11-19 12:31:43 +0530

    Added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ': It is currently ' + body.current.temperature + ' degrees outside and it feel like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, body.current.weather_descriptions[0] + ': It is currently ' + body.current.temperature + ' degrees outside and it feel like ' + body.current.feelslike + ' degrees. Humidity is ' + body.current.humidity + '%.')
         }
     })
 }
