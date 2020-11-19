dc1e3e73931afa4843038210aac271cfd4e35d91
Lineesh NM <lineesh2009@gmail.com>
2020-11-19 10:59:54 +0530

    removed unnecessory console.log

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,6 @@ weatherForm.addEventListener('submit', (e) => {
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecast
         }
-
+        })
     })
-})
 })
